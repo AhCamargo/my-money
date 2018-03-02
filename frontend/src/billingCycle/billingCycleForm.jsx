161052a26c8c85e1ba18576e39f62b261f82ab6f
acamargo2017 <andre.camargo500@gmail.com>
2018-03-02 17:08:53 -0300

    Alterado CredList para ItemList

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { reduxForm, Field } from 'redux-form'
+import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 
 
@@ -13,7 +13,7 @@ import CreditList from './creditList'
 class BillingCycleForm extends Component {
 
     render() {
-        const { handleSubmit, readOnly } = this.props
+        const { handleSubmit, readOnly, credits, debts } = this.props
     
         return (
           <form role='form' onSubmit={handleSubmit}>
@@ -24,8 +24,12 @@ class BillingCycleForm extends Component {
                      label='Mês' cols='12 4' placeholder='Informe o mês' />
                <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly}
                      label='Ano' cols='12 4' placeholder='Informe o ano' />
-               <CreditList cols='12 6' readOnly={readOnly} />     
+               <ItemList cols='12 6' list={credits} readOnly={readOnly}
+                    field='credits' legend='Créditos' />
+               <ItemList cols='12 6' list={debts} readOnly={readOnly}
+                    field='debts' legend='Débitos' showStatus={true} />     
              </div>
+
              <div className='box-footer'>
                 <button type='submit' className={`btn btn-${this.props.submitClass}`}>
                      {this.props.submitLabel}
@@ -42,6 +46,11 @@ class BillingCycleForm extends Component {
 BillingCycleForm =  reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
 // destroyOnUnmount = para nao destruir o dado(estado) do formulário.
 // (Ciclo de Pagamento) quando  clicar em alterar, ele já volta com os dados cadastrado antes.
+const selector = formValueSelector('billingCycleForm')
 
+const mapStateToProps = state => ({
+      credits: selector(state, 'credits'),
+      debts: selector(state, 'debts')
+})
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
-export default connect(null, mapDispatchToProps)(BillingCycleForm)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
