4f6a8f2a4f88a8a07581c4dd3e9e5917ed938563
acamargo2017 <andre.camargo500@gmail.com>
2018-03-02 14:35:35 -0300

    Adicionado CreditList

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
-import { reset as resetForm } from 'redux-form'
+import { reset as resetForm, initialize } from 'redux-form'
 import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:1991/api'
+const INITIAL_VALUES = {}
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -14,17 +15,24 @@ export function getList() {
 }
 
 export function create(values) {
-    return dispatch => {
+    return submit(values, 'post')
+}
 
-        axios.post(`${BASE_URL}/billingCycles`, values)
+export function update(values) {
+     return submit(values, 'put')
+}
+
+export function remove(values) {
+    return submit(values, 'delete')
+}
+
+function submit(values, method) {
+    return dispatch => {
+        const id = values._id ? values._id : '' 
+        axios[method](`${BASE_URL}/billingCycles/${id}`, values)
          .then(resp =>{
              toastr.success('Sucesso', 'Operação Realizada com sucesso.')
-             dispatch([
-                 resetForm('billingCycleForm'),
-                 getList(),
-                 selectTab('tabList'),
-                 showTabs('tabList', 'tabCreate')
-             ])
+             dispatch(init())
          })
          .catch(e => {
              e.response.data.errors.forEach(error => toastr.error('Erro', error))
@@ -32,12 +40,31 @@ export function create(values) {
          })
 
     }
-    
+
 }
 
 export function showUpdate(billingCycle) {
     return [
         showTabs('tabUpdate'),
-        selectTab('tabUpdate')
+        selectTab('tabUpdate'),
+        initialize('billingCycleForm', billingCycle)
+    ]
+}
+
+export function showDelete(billingCycle) {
+    return [
+        showTabs('tabDelete'),
+        selectTab('tabDelete'),
+        initialize('billingCycleForm', billingCycle)
+    ]
+}
+
+// Sempre que eu sair do cadastro e voltar, quero que as funcoes estejam ativas
+export function init() {
+    return [
+        showTabs('tabList', 'tabCreate'),
+        selectTab('tabList'),
+        getList(), //pega a lista atualizada
+        initialize('billingCycleForm', INITIAL_VALUES) // inicializa com os valores iniciais
     ]
 }
